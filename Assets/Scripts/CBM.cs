ae60c093bf4acd01917e7da4df3b4ea415b2565f
Armando Ponce Soriano <aponcesoriano@hotmail.com>
2023-06-03 19:33:58 -0600

    Uso de ScriptableObjects implementado, instanciado de objetos en vista AR de Building

---
+++ Assets/Scripts/CBM.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class CBM : MonoBehaviour
+{
+    public string ComponentName;
+    public string ComponentDesc;
+    public Sprite ComponentImage;
+    public GameObject ComponentModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = ComponentName;
+        transform.GetChild(1).GetComponent<RawImage>().texture = ComponentImage.texture;
+        transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = ComponentDesc;
+
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(AppManager.instance.ARPosition);
+        button.onClick.AddListener(Create3DModel);
+    }
+
+    private void Create3DModel()
+    {
+        Instantiate(ComponentModel);
+    }
+
+}
---
+++ Assets/Scripts/Component.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Component : ScriptableObject
+{
+    public string ComponentName;
+    public Sprite ComponentImage;
+    public string ComponentDesc;
+    public GameObject ComponentModel;
+}
